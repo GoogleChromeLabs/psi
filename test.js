7703ef649feb11c0d0f378e18919726ed8220562
Addy Osmani <addyosmani@gmail.com>
2014-06-03 20:58:10 +0100

    Minor clean-up of tests

--- test.js
+++ test.js
@@ -2,13 +2,12 @@
 var fs = require('fs');
 var assert = require('assert');
 var insights = require('./index');
+var key = 'AIzaSyCHBBOqcgSVUC_shyK6BEAKOZZoBpJCF6g';
 
 it('should throw if no valid URL is provided', function (done) {
-	var testData = ({
-		key: 'AIzaSyCHBBOqcgSVUC_shyK6BEAKOZZoBpJCF6g'
-	});
-
-	insights(testData, function(err, data){
+	insights({
+		key: key
+	}, function(err, data){
 	    if(/Invalid url/.test(err)) {
 	        return true;
 	    }
@@ -16,14 +15,13 @@ it('should throw if no valid URL is provided', function (done) {
 	});
 });
 
-it('should return desktop results from PageSpeed Insights', function (done) {
-	var testData = ({
-		key: 'AIzaSyCHBBOqcgSVUC_shyK6BEAKOZZoBpJCF6g',
+it('should return desktop results from PageSpeed Insights', function (done) {	
+	insights({
+		key: key,
 		url: 'http://html5rocks.com',
 		strategy: 'desktop',
 		threshold: 80
-	});	
-	insights(testData, function(err, data){
+	}, function(err, data){
 		assert(data.responseCode == 200);
 		assert(data.id == 'http://www.html5rocks.com/en/');
 		done();
@@ -31,14 +29,13 @@ it('should return desktop results from PageSpeed Insights', function (done) {
 });
 
 it('should return mobile results from PageSpeed Insights', function (done) {
-	var testData = ({
-		key: 'AIzaSyCHBBOqcgSVUC_shyK6BEAKOZZoBpJCF6g',
+	insights({
+		key: key,
 		url: 'http://html5rocks.com',
 		locale: 'en_GB',
 		strategy: 'mobile',
 		threshold: 80
-	});	
-	insights(testData, function(err, data){
+	}, function(err, data){
 		assert(data.score == 73);
 		assert(data.responseCode == 200);
 		assert(data.id == 'http://www.html5rocks.com/en/');
