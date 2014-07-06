3791d31c2c5e4f00dfeed832b384e26ba5a63e49
James Cryer <info@jamescryer.com>
2014-07-07 00:22:17 +0100

    Adding chalk to output

--- test/utils.js
+++ test/utils.js
@@ -2,13 +2,14 @@
 'use strict';
 
 var assert = require('assert');
+var chalk = require('chalk');
 var utils = require('../lib/utils');
 
 describe('Report utils', function() {
 
   it('should create a divider', function() {
     var expected = '\n----------------------------------------------------------------\n';
-    assert.equal(utils.divider, expected);
+    assert.equal(chalk.stripColor(utils.divider), expected);
   });
 
   it('should create buffer space', function() {
@@ -33,6 +34,24 @@ describe('Report utils', function() {
   it('should create label', function() {
     var expected = 'Enable Gzip Compression                          | ';
     var msg = 'enableGzipCompression';
-    assert.equal(utils.labelize(msg), expected);
+    assert.equal(chalk.stripColor(utils.labelize(msg)), expected);
+  });
+
+  it('should select red for scores 20 or less', function() {
+    var expected = chalk.red;
+    var score = 20;
+    assert.equal(utils.scoreColor(score), expected);
+  });
+
+  it('should select yellow for scores between 21 and 79', function() {
+    var expected = chalk.yellow;
+    var score = 50;
+    assert.equal(utils.scoreColor(score), expected);
+  });
+
+  it('should select green for scores greater than 80', function() {
+    var expected = chalk.green;
+    var score = 80;
+    assert.equal(utils.scoreColor(score), expected);
   });
 });
