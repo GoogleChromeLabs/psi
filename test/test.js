d2edd0e1ef0c9db9c1821e11861867f303b905e6
Sam Saccone <samccone@gmail.com>
2014-07-06 17:17:23 -0400

    Stub out gpagespeed

--- test/test.js
+++ test/test.js
@@ -1,40 +1,41 @@
 'use strict';
-var fs = require('fs');
-var assert = require('assert');
-var insights = require('../index');
 
-it('should throw if no valid URL is provided', function (done) {
-	insights({}
-	, function(err, data){
-	    if(/Invalid url/.test(err)) {
-	        return true;
-	    }
-		done();
-	});
+var assert    = require('assert');
+var insights  = require('../index');
+var Output    = require('../output');
+var fs        = require('fs');
+var Path      = require('path');
+
+describe("PSI formatting", function () {
+  beforeEach(function () {
+    var World            = this;
+    this.Log             = console.log;
+    this.output          = Output.init();
+    this.response        = require('./fixtures/response');
+    this.Output          = fs.readFileSync(Path.join(__dirname, "fixtures/output.txt"), "utf8")
+    this.formattedOutput = "";
+
+    console.log = function(content) {
+      World.formattedOutput += content + "\n"
+      this.Log(content);
+    }.bind(this);
+  });
+
+  afterEach(function () {
+    console.log = this.Log;
+  });
+
+  it('should correctly format PageSpeed Insights response', function () {
+    this.output.process({strategy: "desktop"}, this.response);
+    assert.equal(this.formattedOutput, this.Output);
+  });
 });
 
-it('should return desktop results from PageSpeed Insights', function (done) {
-	insights({
-		url: 'http://html5rocks.com',
-		strategy: 'desktop',
-		threshold: 80
-	}, function(err, data){
-		assert.equal(data.responseCode, 200, "responseCode");
-		assert.equal(data.id, 'http://www.html5rocks.com/en/', "id");
-		done();
-	});
-});
-
-it('should return mobile results from PageSpeed Insights', function (done) {
-	insights({
-		url: 'http://html5rocks.com',
-		locale: 'en_GB',
-		strategy: 'mobile',
-		threshold: 73
-	}, function(err, data){
-		assert.equal(data.score, 73, "score");
-		assert.equal(data.responseCode, 200, "responseCode");
-		assert.equal(data.id, 'http://www.html5rocks.com/en/', "id");
-		done();
-	});
+describe("CLI Error handling", function() {
+  it('should throw if no valid URL is provided', function (done) {
+    insights({}, function(err, data){
+      assert.equal(err.message, "Missing required param: url");
+      done()
+    });
+  });
 });
