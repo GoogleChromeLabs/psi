c21ad98b265049fe370385d0052a5938a2e1bf29
James Cryer <info@jamescryer.com>
2014-07-07 20:03:57 +0100

    Update tests for chalk output

--- test/test.js
+++ test/test.js
@@ -1,22 +1,24 @@
+/* global describe, beforeEach, afterEach, it */
 'use strict';
 
 var assert    = require('assert');
+var chalk     = require('chalk');
 var insights  = require('../index');
 var Output    = require('../output');
 var fs        = require('fs');
 var Path      = require('path');
 
-describe("PSI formatting", function () {
+describe('PSI formatting', function () {
   beforeEach(function () {
     var World            = this;
     this.Log             = console.log;
     this.output          = Output.init();
     this.response        = require('./fixtures/response');
-    this.Output          = fs.readFileSync(Path.join(__dirname, "fixtures/output.txt"), "utf8")
-    this.formattedOutput = "";
+    this.Output          = fs.readFileSync(Path.join(__dirname, 'fixtures/output.txt'), 'utf8');
+    this.formattedOutput = '';
 
     console.log = function(content) {
-      World.formattedOutput += content + "\n"
+      World.formattedOutput += content + '\n';
       this.Log(content);
     }.bind(this);
   });
@@ -26,16 +28,16 @@ describe("PSI formatting", function () {
   });
 
   it('should correctly format PageSpeed Insights response', function () {
-    this.output.process({strategy: "desktop"}, this.response);
-    assert.equal(this.formattedOutput, this.Output);
+    this.output.process({strategy: 'desktop'}, this.response);
+    assert.equal(chalk.stripColor(this.formattedOutput), this.Output);
   });
 });
 
-describe("CLI Error handling", function() {
+describe('CLI Error handling', function() {
   it('should throw if no valid URL is provided', function (done) {
-    insights({}, function(err, data){
-      assert.equal(err.message, "Missing required param: url");
-      done()
+    insights({}, function(err) {
+      assert.equal(err.message, 'Missing required param: url');
+      done();
     });
   });
 });
