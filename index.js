cabc88babbfd686816b9e86937000f7a3fcdbeb9
Sakesan Panjamawat <sakp@venda.com>
2014-10-13 17:03:54 +0700

    use json.parse only when returning data is string

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ module.exports = function (opts, cb) {
       return;
     }
 
-    var response = JSON.parse(data);
+    var response = (typeof data === "object")?data:JSON.parse(data);
     output.process(opts, response, function(processErr) {
       cb(processErr || err, response);
     });
