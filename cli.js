fde31192568d7459d9127975cedd79ae93e02809
Sindre Sorhus <sindresorhus@gmail.com>
2015-01-09 13:40:01 +0800

    rewrite CLI
    less code and better help output
    
    also update notifier

--- cli.js
+++ cli.js
@@ -1,69 +1,43 @@
 #!/usr/bin/env node
 'use strict';
-var pkg = require('./package.json');
-var argv = require('minimist')((process.argv.slice(2)));
-var insights = require('./');
-var query = process.argv[2];
-var opts = {};
+var meow = require('meow');
+var updateNotifier = require('update-notifier');
+var psi = require('./');
 
-function printHelp() {
-  console.log(pkg.description);
-  console.log('');
-  console.log('Usage:');
-  console.log('  $ psi <url> --key=<key>');
-  console.log('');
-  console.log('Optional, supply other arguments.');
-  console.log('See https://developers.google.com/speed/docs/insights/v1/getting_started for description');
-  console.log('  $ psi <url> --key=<key> --prettyprint=<true> --userIp=<userIp> --locale=<locale> --strategy=<desktop|mobile> --format=<cli|json|tap>');
+var cli = meow({
+    help: [
+        'Usage',
+        '  a11y <url>',
+        '',
+        'Example',
+        '  a11y todomvc.com',
+        '',
+        'Options',
+        '  --key          Google API Key. By default the free tier is used.',
+        '  --format       Output format: cli|json|tap',
+        '  --strategy     Strategy to use when analyzing the page: desktop|mobile',
+        '  --prettyprint  Pretty print the result.',
+        '  --locale       Locale results should be generated in.'
+    ].join('\n')
+});
+
+updateNotifier({
+    packageName: cli.pkg.name,
+    packageVersion: cli.pkg.version
+}).notify();
+
+if (!cli.input[0]) {
+    console.error('Please supply an URL');
+    process.exit(1);
 }
 
-if (!query || process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help') !== -1) {
-  printHelp();
-  return;
-}
+cli.flags.url = cli.input[0];
 
-if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--version') !== -1) {
-  console.log(pkg.version);
-  return;
-}
+psi(cli.flags, function (err) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
-opts.url = argv._[0];
-
-if (!opts.url) {
-  printHelp();
-  return;
-}
-
-if (argv.url) {
-  opts.url = argv.url;
-}
-
-if (argv.key) {
-  opts.key = argv.key;
-}
-
-if (argv.callback) {
-  opts.callback = argv.callback;
-}
-
-if (argv.prettyprint) {
-  opts.prettyprint = argv.prettyprint;
-}
-
-if (argv.userIp) {
-  opts.userIp = argv.userIp;
-}
-
-if (argv.locale) {
-  opts.locale = argv.locale;
-}
-
-if (argv.strategy) {
-  opts.strategy = argv.strategy;
-}
-
-if (argv.format) {
-  opts.format = argv.format;
-}
-
-insights(opts);
+  process.exit(0);
+});
