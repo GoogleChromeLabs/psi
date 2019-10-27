e95fe41fb8d44e66125fbc07da9e70ac6047c6a6
JuanMa Ruiz <ruizjuanma@gmail.com>
2019-10-27 12:48:32 +0100

    Added test to check handleOpts method in index file

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,9 @@
 /* eslint-env mocha */
 const {expect} = require('chai');
+const rewire = require('rewire');
+
+const psiWired = rewire('..');
+const handleOptions = psiWired.__get__('handleOpts');
 
 const psi = require('..');
 
@@ -10,5 +14,25 @@ describe('Index file', () => {
       .catch(error => {
         expect(error.message).to.eql('URL required');
       });
+  });
+  describe('handleOpts method', () => {
+    const defaultConfig = {
+      nokey: true,
+      url: 'http://addyosmani.com'
+    };
+    it('should return strategy "mobile" if no strategy is passed', () => {
+      const expectedOutput = {
+        ...defaultConfig,
+        strategy: 'mobile'
+      };
+      expect(handleOptions('http://addyosmani.com')).to.eql(expectedOutput);
+    });
+    it('should return strategy "desktop" if "desktop" is passed as strategy', () => {
+      const expectedOutput = {
+        ...defaultConfig,
+        strategy: 'desktop'
+      };
+      expect(handleOptions('http://addyosmani.com', {strategy: 'desktop'})).to.eql(expectedOutput);
+    });
   });
 });
