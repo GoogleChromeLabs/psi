6d38bed927f9a120ca1d0e074d4d8b1dd82855cd
JuanMa Ruiz <ruizjuanma@gmail.com>
2020-08-27 20:36:33 +0200

    Add improvements from code review
    * Negative assertion on string-utils.test.
    * More readable regexp with named groups.
    * Fix typo in cli.js help

--- test/string-utils.test.js
+++ test/string-utils.test.js
@@ -9,5 +9,10 @@ describe('stringUtils module', () => {
        have finished loading to lower time to interactive. [Learn more](https://web.dev/offscreen-images).`;
       expect(getLink(str)).to.eql('https://web.dev/offscreen-images');
     });
+    it('should return an empty string when it does not contain an url', () => {
+      const str = `Consider lazy - loading offscreen and hidden images after all critical resources
+       have finished loading to lower time to interactive.`;
+      expect(getLink(str)).to.eql('');
+    });
   });
 });
