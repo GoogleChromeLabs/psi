65997884feeb032be18261c56f5208956bb49690
JuanMa Ruiz <ruizjuanma@gmail.com>
2021-01-06 20:19:02 +0100

    Fix linter errors

--- test/api-response.test.js
+++ test/api-response.test.js
@@ -7,13 +7,13 @@ describe('API', function () {
   this.timeout(50000);
 
   it('should get data from PageSpeed Insights', () => {
-    return psi('https://hn.svelte.dev/').then(data => {
+    return psi('addyosmani.com/').then(data => {
       assert.include(data.data.id, 'addyosmani.com');
     });
   });
 
   it('should support options', () => {
-    return psi('https://hn.svelte.dev/', {locale: 'no'}).then(data => {
+    return psi('addyosmani.com/', {locale: 'no'}).then(data => {
       assert.strictEqual(data.data.lighthouseResult.configSettings.locale, 'no');
     });
   });
